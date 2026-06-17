910489750ca19f5f5719414313df6190d8eff654
femabeh <femabeh@gmail.com>
2026-06-17 09:32:26 +0200

    Added workspace-display-picker component. Now upgrading to new angular version.

---
+++ frontend/src/app/components/workspace-display-picker/workspace-display-picker.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-workspace-display-picker',
+  imports: [],
+  templateUrl: './workspace-display-picker.html',
+  styleUrl: './workspace-display-picker.scss',
+})
+export class WorkspaceDisplayPicker {
+
+}

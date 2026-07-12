a171ee497a395f61919cc9aabf025849a78833d3
femabeh <femabeh@gmail.com>
2026-07-12 13:45:27 +0200

    Removed test endpoint

--- backend/src/router/root.py
+++ backend/src/router/root.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 
 import config
-from utils.auth import CurrentUser
 
 router = APIRouter(tags=["ROOT"])
 
@@ -20,8 +19,3 @@ def get_root():
 @router.get("/auth_config")
 def get_auth_config():
     return config.AUTH_CONFIG
-
-
-@router.get("/auth_test")
-async def get_auth_test(user: CurrentUser):
-    print(user)

c5067bd0af4ab0971e07af3b76eed55c0ff1a3b4
femabeh <femabeh@gmail.com>
2026-07-12 13:47:16 +0200

    Removed endpoint call

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,7 +2,6 @@ import {ChangeDetectionStrategy, Component, inject, OnInit} from '@angular/core'
 import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
 import {initFlowbite} from 'flowbite';
 import {WorkspaceSelection} from 'components/workspace-selection/workspace-selection';
-import {Workspace} from 'services/workspace';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +12,8 @@ import {Workspace} from 'services/workspace';
 })
 export class App implements OnInit {
   private readonly router = inject(Router);
-  private readonly workspaceService = inject(Workspace);
 
   ngOnInit() {
-    this.workspaceService.auth_test();
-
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         initFlowbite();
--- frontend/src/app/services/workspace.ts
+++ frontend/src/app/services/workspace.ts
@@ -4,10 +4,4 @@ import {HttpClient} from '@angular/common/http';
 @Service()
 export class Workspace {
   private readonly http = inject(HttpClient);
-
-  auth_test() {
-    this.http.get('http://localhost:8000/auth_test').subscribe((result) => {
-      console.log(result);
-    })
-  }
 }
